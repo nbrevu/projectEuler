c9c1c59bcf2cced60a3bed0d4ffc65acfffc93c0
Pablo Moreno <pablo.moreno.olalla@gmail.com>
2019-09-26 05:26:11 +0200

    [Euler41] Now with Miller-Rabin. Or Rabin-Miller.

---
+++ ProjectEuler/src/main/java/com/euler/Euler41.java
@@ -0,0 +1,37 @@
+package com.euler;
+
+import java.math.BigInteger;
+
+import com.euler.common.EulerUtils.IntPermutation;
+import com.euler.common.EulerUtils.IntPermutationGenerator;
+import com.euler.common.Primes.RabinMiller;
+import com.euler.common.Timing;
+import com.koloboke.collect.set.IntSet;
+import com.koloboke.collect.set.hash.HashIntSets;
+
+public class Euler41 {
+	private static int transform(int[] digits)	{
+		int reverse=digits.length;
+		int result=0;
+		for (int d:digits)	{
+			result*=10;
+			result+=reverse-d;
+		}
+		return result;
+	}
+	
+	private static long solve()	{
+		IntSet witnesses=HashIntSets.newImmutableSetOf(31,73);
+		RabinMiller tester=new RabinMiller();
+		for (int i=7;;i-=3) for (IntPermutation perm:new IntPermutationGenerator(i))	{
+			int[] base=perm.getNumbers();
+			if ((base[0]%2)==1) continue;
+			int candidate=transform(base);
+			if (tester.isPrime(BigInteger.valueOf(candidate),witnesses)) return candidate;
+		}
+	}
+	
+	public static void main(String[] args)	{
+		Timing.time(Euler41::solve);
+	}
+}
