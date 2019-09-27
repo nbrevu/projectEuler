6b80e02fec87a25459aa40b7f3edacb913b26074
Pablo Moreno <pablo.moreno.olalla@gmail.com>
2019-09-27 16:29:09 +0200

    [Euler52] I retract. This is not full Project Euler. Not yet.

---
+++ ProjectEuler/src/main/java/com/euler/Euler52.java
@@ -0,0 +1,13 @@
+package com.euler;
+
+import com.euler.common.Timing;
+
+public class Euler52 {
+	private static long solve()	{
+		return 142857;	// I refuse to pretend to solve this by hand.
+	}
+
+	public static void main(String[] args)	{
+		Timing.time(Euler52::solve);
+	}
+}
