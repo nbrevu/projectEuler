cb6640a859c725147a9f583e12b7289e20b1cba1
Pablo Moreno <pablo.moreno.olalla@gmail.com>
2019-09-30 03:55:58 +0200

    [Euler75] Pythagorean triples are back, in pog form!

---
+++ ProjectEuler/src/main/java/com/euler/Euler75.java
@@ -0,0 +1,24 @@
+package com.euler;
+
+import com.euler.common.PythagoreanTriples;
+import com.euler.common.PythagoreanTriples.SimplePythagoreanTriple;
+import com.euler.common.Timing;
+
+public class Euler75 {
+	private final static int LIMIT=1500000;
+	
+	public static long solve()	{
+		int[] found=new int[1+LIMIT];
+		for (SimplePythagoreanTriple triple:PythagoreanTriples.getSimpleTriplesWithPerimeterLimit(LIMIT))	{
+			int perimeter=(int)(triple.a+triple.b+triple.c);
+			for (int i=perimeter;i<=LIMIT;i+=perimeter) ++found[i];
+		}
+		int result=0;
+		for (int i=12;i<=LIMIT;++i) if (found[i]==1) ++result;
+		return result;
+	}
+	
+	public static void main(String[] args)	{
+		Timing.time(Euler75::solve);
+	}
+}
